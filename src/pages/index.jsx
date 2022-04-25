2e3b1c9b298c3f3887e08bed6b0f7495785ad2c0
Pete Davis <davispete@gmail.com>
2022-04-24 20:02:08 -0700

    tutorial part II

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this cool site with the Gatsby tutorial.</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default IndexPage
